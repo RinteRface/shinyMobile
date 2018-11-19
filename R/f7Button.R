71f86f9ef2357f570ff72fdf06e1637f9bc12f86
Divad0175 <dgranjon@ymail.com>
2018-11-19 16:22:41 +0100

    add button

--- R/f7Button.R
+++ R/f7Button.R
@@ -1,36 +1,53 @@
-library(shinyF7)
-library(htmltools)
-
-f7Button <- function(..., color = NULL, fill = FALSE, outline = FALSE,
-                     raised = FALSE, segmented = FALSE, round = FALSE,
-                     big = FALSE, small = FALSE) {
+#' Create a Framework7 button
+#'
+#' Build a Framework7 button
+#'
+#' @param ... Button text.
+#' @param src Button link.
+#' @param color Button color. See here for valid colors \url{https://framework7.io/docs/badge.html}.
+#' @param fill Fill style. FALSE by default.
+#' @param outline Outline style. FALSE by default.
+#' @param shadow Button shadow. FALSE by default. Only for material design.
+#' @param rounded Round style. FALSE by default.
+#' @param size Button size. NULL by default but also "big" or "small".
+#'
+#' @note Not ready for production
+#'
+#' @examples
+#' if(interactive()){
+#'  library(shiny)
+#'  library(shinyF7)
+#'
+#'  shiny::shinyApp(
+#'   ui = f7Page(
+#'     title = "My app",
+#'     f7Button(color = "blue", "My button", src = "https://www.google.com"),
+#'     f7Button(color = "red", "My button", src = "https://www.google.com", fill = TRUE)
+#'   ),
+#'   server = function(input, output) {}
+#'  )
+#' }
+#'
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @export
+f7Button <- function(..., src, color = NULL, fill = FALSE, outline = FALSE,
+                     shadow = FALSE, rounded = FALSE, size = NULL) {
 
   buttonCl <- "button"
   if (!is.null(color)) buttonCl <- paste0(buttonCl, " color-", color)
   if (fill) buttonCl <- paste0(buttonCl, " button-fill")
   if (outline) buttonCl <- paste0(buttonCl, " button-outline")
-  if (raised) buttonCl <- paste0(buttonCl, " button-raised")
-  if (segmented) buttonCl <- paste0(buttonCl, " button-segmented")
-  if (round) buttonCl <- paste0(buttonCl, " button-round")
-  if (big) buttonCl <- paste0(buttonCl, " button-big")
-  if (small) buttonCl <- paste0(buttonCl, " button-small")
+  if (shadow) buttonCl <- paste0(buttonCl, " button-raised")
+  if (rounded) buttonCl <- paste0(buttonCl, " button-round")
+  if (!is.null(size)) buttonCl <- paste0(buttonCl, " button-", size)
 
 
-  buttonTag <- shiny::tags$button(
+  buttonTag <- shiny::tags$a(
     class = buttonCl,
-      shiny::tags$a(class = "bottom",
-      ...
-    )
+    href = src,
+    target = "_blank",
+    ...
   )
-
   return(buttonTag)
 }
-
-
-shiny::shinyApp(
-  ui = f7Page(
-    title = "My app",
-    f7Button(color = "blue", "my Button"),
-  ),
-  server = function(input, output) {}
-)
