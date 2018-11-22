8cf3efe75072b10780d6f021ca64de4fdf4d73a4
Divad0175 <dgranjon@ymail.com>
2018-11-22 17:01:04 +0100

    rebuilt website

--- R/f7Gauge.R
+++ R/f7Gauge.R
@@ -2,7 +2,6 @@
 #'
 #' Build a Framework7 gauge
 #'
-#' @param ... Any text.
 #' @param id Gauge ID.
 #' @param type Gauge type. Can be "circle" or "semicircle". Default is "circle."
 #' @param value Gauge value/percentage. Must be a number between 0 and 1. Default is 0.
@@ -11,10 +10,10 @@
 #' @param borderBgColor Main border/stroke background color.
 #' @param borderColor Main border/stroke color.
 #' @param borderWidth Main border/stroke width.
-#' @param text Gauge value text (large text in the center of gauge).
-#' @param textColor Value text color.
-#' @param fontSize Value text font size.
-#' @param fontWeight Value text font weight.
+#' @param valueText Gauge value text (large text in the center of gauge).
+#' @param valueTextColor Value text color.
+#' @param valueFontSize Value text font size.
+#' @param valueFontWeight Value text font weight.
 #' @param labelText Gauge additional label text.
 #' @param labelTextColor Label text color.
 #' @param labelFontSize Label text font size.
@@ -28,9 +27,17 @@
 #'  shiny::shinyApp(
 #'   ui = f7Page(
 #'     title = "My app",
-#'     f7Gauge(id = "ID", type  = "semircircle", value = 10,
-#'       textcolor = "#2196f3", bgcolor = "#2196f3",
-#'       text = "Hello", label = "label")
+#'     f7Gauge(
+#'      id = "mygauge",
+#'      type  = "semicircle",
+#'      value = 10,
+#'      borderColor = "#2196f3",
+#'      borderWidth = 10,
+#'      valueText = "50%",
+#'      valueFontSize = 41,
+#'      valueTextColor = "#2196f3",
+#'      labelText = "amount of something"
+#'     )
 #'   ),
 #'   server = function(input, output) {}
 #'  )
@@ -41,36 +48,80 @@
 #' @author David Granjon and Isabelle Rudolf, \email{dgranjon@@ymail.com}
 #'
 #' @export
-f7Gauge <- function(..., id, type = NULL, value = NULL, size = NULL, bgColor = NULL,
+f7Gauge <- function(id, type = NULL, value = NULL, size = NULL, bgColor = NULL,
                     borderBgColor = NULL, borderColor = NULL, borderWidth = NULL,
-                    text = NULL, textColor = NULL, fontSize = NULL,
-                    fontWeight = NULL, labelText = NULL, labelTextColor = NULL,
+                    valueText = NULL, valueTextColor = NULL, valueFontSize = NULL,
+                    valueFontWeight = NULL, labelText = NULL, labelTextColor = NULL,
                     labelFontSize = NULL, labelFontWeight = NULL) {
 
   gaugeCl <- "gauge gauge-init"
 
+  # gaugeCl <- paste0("gauge ", id)
+  #
+  # gaugeProps <- dropNulls(
+  #   list(
+  #     el = id,
+  #     type = type,
+  #     size = size,
+  #     bgColor = bgColor,
+  #     borderBgColor = borderBgColor,
+  #     borderColor = borderColor,
+  #     borderWidth = borderWidth,
+  #     valueText = valueText,
+  #     valueTextColor = valueTextColor,
+  #     valueFontSize = valueFontSize,
+  #     valueFontWeight = valueFontWeight,
+  #     labelText = labelText,
+  #     labelTextColor = labelTextColor,
+  #     labelFontSize = labelFontSize,
+  #     labelFontWeight = labelFontWeight
+  #   )
+  # )
+  #
+  # gaugeProps <- as.data.frame(gaugeProps)
+  #
+  # gaugeProps <- jsonlite::toJSON(gaugeProps)
+  # gaugeProps <- gsub(x = gaugeProps, pattern = "\\[", replacement = "")
+  # gaugeProps <- gsub(x = gaugeProps, pattern = "\\]", replacement = "")
+  #
+  # gaugeJS <- shiny::tags$script(
+  #   paste0(
+  #     "var gauge", id, " = app.gauge.create(
+  #       ", gaugeProps,"
+  #      );
+  #     "
+  #   )
+  # )
+  #
+  # gaugeTag <- shiny::tags$div(class = gaugeCl)
+
+  #shiny::tagList(
+  #  shiny::singleton(shiny::tags$head(gaugeJS)),
+  #  gaugeTag
+  #)
+
   innerTag <- dropNulls(
-    list(
-      shiny::tags$div(
-        id = id,
-        class = gaugeCl,
-        `data-type` = type,
-        `data-value` = value,
-        `data-size` = size,
-        `data-bg-color` = bgColor,
-        `data-border-bg-color` = borderBgColor,
-        `data-border-color` = borderColor,
-        `data-border-width` = borderWidth,
-        `data-value-text` = text,
-        `data-value-text-color` = textColor,
-        `data-value-text-font-size` = fontSize,
-        `data-value-text-font-weight` = fontWeight,
-        `data-label-text` = labelText,
-        `data-label-text-color` = labelTextColor,
-        `data-label-font-size` = labelFontSize,
-        `data-label-font-weight` = labelFontWeight
-      )
-    )
+   list(
+     shiny::tags$div(
+       id = id,
+       class = gaugeCl,
+       `data-type` = type,
+       `data-value` = value,
+       `data-size` = size,
+       `data-bg-color` = bgColor,
+       `data-border-bg-color` = borderBgColor,
+       `data-border-color` = borderColor,
+       `data-border-width` = borderWidth,
+       `data-value-text` = valueText,
+       `data-value-text-color` = valueTextColor,
+       `data-value-text-font-size` = valueFontSize,
+       `data-value-text-font-weight` = valueFontWeight,
+       `data-label-text` = labelText,
+       `data-label-text-color` = labelTextColor,
+       `data-label-font-size` = labelFontSize,
+       `data-label-font-weight` = labelFontWeight
+     )
+   )
   )
 
   innerTag
--- R/f7Page.R
+++ R/f7Page.R
@@ -84,7 +84,43 @@ f7Page <- function(..., title = NULL, dark_mode = FALSE, color = NULL){
 #' if(interactive()){
 #'  library(shiny)
 #'  library(shinyF7)
-#'
+#'  shiny::shinyApp(
+#'   ui = f7Page(
+#'     setTheme("ios"),
+#'     title = "My app",
+#'     f7SingleLayout(
+#'       navbar = f7Navbar(
+#'         title = "Single Layout",
+#'         hairline = FALSE,
+#'         shadow = TRUE
+#'       ),
+#'       toolbar = f7Toolbar(
+#'         f7Link(label = "Link 1", src = "https://www.google.com"),
+#'         f7Link(label = "Link 2", src = "https://www.google.com", external = TRUE)
+#'       ),
+#'       # main content
+#'       f7Shadow(
+#'         intensity = 10,
+#'         hover = TRUE,
+#'         f7Card(
+#'           title = "Card header",
+#'           sliderInput("obs", "Number of observations", 0, 1000, 500),
+#'           plotOutput("distPlot"),
+#'           footer = tagList(
+#'             f7Button(color = "blue", "My button", src = "https://www.google.com"),
+#'             f7Badge("Badge", color = "green")
+#'           )
+#'         )
+#'       )
+#'     )
+#'   ),
+#'   server = function(input, output) {
+#'     output$distPlot <- renderPlot({
+#'       dist <- rnorm(input$obs)
+#'       hist(dist)
+#'     })
+#'   }
+#'  )
 #' }
 #'
 #' @author David Granjon, \email{dgranjon@@ymail.com}
@@ -121,6 +157,97 @@ f7SingleLayout <- function(..., navbar, toolbar, navbarHideScroll = FALSE) {
 #'  library(shiny)
 #'  library(shinyF7)
 #'
+#'  shiny::shinyApp(
+#'   ui = f7Page(
+#'     title = "My app",
+#'     f7TabLayout(
+#'       setTheme("ios"),
+#'       f7Panel(title = "Left Panel", side = "left", theme = "light", "Blabla", style = "cover"),
+#'       f7Panel(title = "Right Panel", side = "right", theme = "dark", "Blabla", style = "cover"),
+#'       f7Navbar(
+#'         title = "Tabs",
+#'         hairline = FALSE,
+#'         shadow = TRUE,
+#'         left_panel = TRUE,
+#'         right_panel = TRUE
+#'       ),
+#'       f7Tabs(
+#'         icons = TRUE,
+#'         animated = TRUE,
+#'         position = "bottom",
+#'         #swipeable = TRUE,
+#'         f7Tab(
+#'           tabName = "Tab 1",
+#'           icon = "email",
+#'           active = TRUE,
+#'           f7Shadow(
+#'             intensity = 10,
+#'             hover = TRUE,
+#'             f7Card(
+#'               title = "Card header",
+#'               sliderInput("obs1", "Number of observations", 0, 1000, 500),
+#'               plotOutput("distPlot1"),
+#'               footer = tagList(
+#'                 f7Button(color = "blue", "My button", src = "https://www.google.com"),
+#'                 f7Badge("Badge", color = "green")
+#'               )
+#'             )
+#'           )
+#'         ),
+#'         f7Tab(
+#'           tabName = "Tab 2",
+#'           icon = "today",
+#'           active = FALSE,
+#'           f7Shadow(
+#'             intensity = 10,
+#'             hover = TRUE,
+#'             f7Card(
+#'               title = "Card header",
+#'               sliderInput("obs2", "Number of observations", 0, 10000, 5000),
+#'               plotOutput("distPlot2"),
+#'               footer = tagList(
+#'                 f7Button(color = "blue", "My button", src = "https://www.google.com"),
+#'                 f7Badge("Badge", color = "green")
+#'               )
+#'             )
+#'           )
+#'         ),
+#'         f7Tab(
+#'           tabName = "Tab 3",
+#'           icon = "file_upload",
+#'           active = FALSE,
+#'           f7Shadow(
+#'             intensity = 10,
+#'             hover = TRUE,
+#'             f7Card(
+#'               title = "Card header",
+#'               sliderInput("obs3", "Number of observations", 0, 10, 5),
+#'               plotOutput("distPlot3"),
+#'               footer = tagList(
+#'                 f7Button(color = "blue", "My button", src = "https://www.google.com"),
+#'                 f7Badge("Badge", color = "green")
+#'               )
+#'             )
+#'           )
+#'         )
+#'       )
+#'     )
+#'   ),
+#'   server = function(input, output) {
+#'     output$distPlot1 <- renderPlot({
+#'       dist <- rnorm(input$obs1)
+#'       hist(dist)
+#'     })
+#'     output$distPlot2 <- renderPlot({
+#'       dist <- rnorm(input$obs2)
+#'       hist(dist)
+#'     })
+#'     output$distPlot3 <- renderPlot({
+#'       dist <- rnorm(input$obs3)
+#'       hist(dist)
+#'     })
+#'   }
+#'  )
 #' }
 #'
 #' @author David Granjon, \email{dgranjon@@ymail.com}
@@ -129,6 +256,7 @@ f7SingleLayout <- function(..., navbar, toolbar, navbarHideScroll = FALSE) {
 f7TabLayout <- function(...) {
   shiny::tags$div(
     class = "page",
+    style="background-color: gainsboro;",
     #shiny::tags$div(class = "panel-backdrop", style = NA),
     ...
   )
@@ -148,6 +276,51 @@ f7TabLayout <- function(...) {
 #'  library(shiny)
 #'  library(shinyF7)
 #'
+#'  shiny::shinyApp(
+#'   ui = f7Page(
+#'     setTheme("ios"),
+#'     title = "My app",
+#'     f7SidebarLayout(
+#'       # sidebar content
+#'       sidebarPanel = f7Sidebar(
+#'         title = "Sidebar",
+#'         side = "left",
+#'         theme = "light",
+#'         sliderInput("obs", "Number of observations", 0, 1000, 500)
+#'       ),
+#'       # main content
+#'       mainPanel = f7mainPanel(
+#'         navbar = f7Navbar(
+#'           title = "Sidebar Layout",
+#'           hairline = FALSE,
+#'           shadow = TRUE
+#'         ),
+#'         toolbar = f7Toolbar(
+#'           f7Link(label = "Link 1", src = "https://www.google.com"),
+#'           f7Link(label = "Link 2", src = "https://www.google.com", external = TRUE)
+#'         ),
+#'         f7Shadow(
+#'           intensity = 10,
+#'           hover = TRUE,
+#'           f7Card(
+#'             title = "Card header",
+#'             plotOutput("distPlot"),
+#'             footer = tagList(
+#'               f7Button(color = "blue", "My button", src = "https://www.google.com"),
+#'               f7Badge("Badge", color = "green")
+#'             )
+#'           )
+#'         )
+#'       )
+#'     )
+#'   ),
+#'   server = function(input, output) {
+#'     output$distPlot <- renderPlot({
+#'       dist <- rnorm(input$obs)
+#'       hist(dist)
+#'     })
+#'   }
+#'  )
 #' }
 #'
 #' @author David Granjon, \email{dgranjon@@ymail.com}
