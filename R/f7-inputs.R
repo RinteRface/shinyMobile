fd1bea8b824b2b80786a612848d7d468cc227f20
Divad0175 <dgranjon@ymail.com>
2019-05-15 12:49:25 +0200

    add f7Sheet

--- R/f7-inputs.R
+++ R/f7-inputs.R
@@ -1,95 +1,95 @@
-#' Create a Framework7 picker input
-#'
-#' Build a Framework7 picker input
-#'
-#' @param inputId Picker input id.
-#' @param label Picker label.
-#' @param placeHolder Text to write in the container.
-#' @param choices Picker choices.
-#'
-#' @examples
-#' if(interactive()){
-#'  library(shiny)
-#'  library(shinyF7)
-#'
-#'  shiny::shinyApp(
-#'    ui = f7Page(
-#'     title = "My app",
-#'     f7pickerInput(
-#'      inputId = "my-picker",
-#'      placeHolder = "Some text here!",
-#'      label = "Picker Input",
-#'      choices = c('a', 'b', 'c')
-#'     )
-#'    ),
-#'    server = function(input, output) {}
-#'  )
-#' }
-#'
-#' @author David Granjon, \email{dgranjon@@ymail.com}
-#'
-#' @export
-f7pickerInput <- function(inputId, label, placeHolder = NULL, choices) {
-
-  # input tag
-  inputTag <- shiny::tags$div(
-    class = "item-content item-input",
-    shiny::tags$div(
-      class = "item-inner",
-      shiny::tags$div(
-        class = "item-input-wrap",
-        shiny::tags$input(
-          type = "text",
-          placeholder = placeHolder,
-          id = inputId
-        )
-      )
-    )
-  )
-
-  # JS
-  choices <- jsonlite::toJSON(choices)
-  choices <- paste("values: ", choices)
-
-  pickerJS <- shiny::tags$script(
-    paste0(
-      "$(function() {
-      var picker = app.picker.create({
-          inputEl: '#", inputId,"',
-          cols: [
-            {
-              textAlign: 'center',
-              ", choices,"
-            }
-          ]
-         });
-      });
-        "
-    )
-  )
-
-  # tag wrapper
-  mainTag <- shiny::tags$div(
-    class = "block-title",
-    label,
-    id = inputId,
-    shiny::tags$div(
-      class = "list no-hairlines-md",
-      shiny::tags$ul(
-        shiny::tags$li(
-          inputTag
-        )
-      )
-    )
-  )
-
-  # final input tag
-  shiny::tagList(
-    shiny::singleton(shiny::tags$head(pickerJS)),
-    mainTag
-  )
-
-}
+# #' Create a Framework7 picker input
+# #'
+# #' Build a Framework7 picker input
+# #'
+# #' @param inputId Picker input id.
+# #' @param label Picker label.
+# #' @param placeHolder Text to write in the container.
+# #' @param choices Picker choices.
+# #'
+# #' @examples
+# #' if(interactive()){
+# #'  library(shiny)
+# #'  library(shinyF7)
+# #'
+# #'  shiny::shinyApp(
+# #'    ui = f7Page(
+# #'     title = "My app",
+# #'     f7pickerInput(
+# #'      inputId = "my-picker",
+# #'      placeHolder = "Some text here!",
+# #'      label = "Picker Input",
+# #'      choices = c('a', 'b', 'c')
+# #'     )
+# #'    ),
+# #'    server = function(input, output) {}
+# #'  )
+# #' }
+# #'
+# #' @author David Granjon, \email{dgranjon@@ymail.com}
+# #'
+# #' @export
+# f7pickerInput <- function(inputId, label, placeHolder = NULL, choices) {
+#
+#   # input tag
+#   inputTag <- shiny::tags$div(
+#     class = "item-content item-input",
+#     shiny::tags$div(
+#       class = "item-inner",
+#       shiny::tags$div(
+#         class = "item-input-wrap",
+#         shiny::tags$input(
+#           type = "text",
+#           placeholder = placeHolder,
+#           id = inputId
+#         )
+#       )
+#     )
+#   )
+#
+#   # JS
+#   choices <- jsonlite::toJSON(choices)
+#   choices <- paste("values: ", choices)
+#
+#   pickerJS <- shiny::tags$script(
+#     paste0(
+#       "$(function() {
+#       var picker = app.picker.create({
+#           inputEl: '#", inputId,"',
+#           cols: [
+#             {
+#               textAlign: 'center',
+#               ", choices,"
+#             }
+#           ]
+#          });
+#       });
+#         "
+#     )
+#   )
+#
+#   # tag wrapper
+#   mainTag <- shiny::tags$div(
+#     class = "block-title",
+#     label,
+#     id = inputId,
+#     shiny::tags$div(
+#       class = "list no-hairlines-md",
+#       shiny::tags$ul(
+#         shiny::tags$li(
+#           inputTag
+#         )
+#       )
+#     )
+#   )
+#
+#   # final input tag
+#   shiny::tagList(
+#     shiny::singleton(shiny::tags$head(pickerJS)),
+#     mainTag
+#   )
+#
+# }
 
 
 
---
+++ R/f7Sheet.R
@@ -0,0 +1,78 @@
+#' Create an f7 sheet modal
+#'
+#' @param ... Sheet content.
+#' @param id Sheet unique id.
+#' @param label Trigger label.
+#' @param orientation "top" or "bottom".
+#'
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'  library(shiny)
+#'  library(shinyF7)
+#'   shiny::shinyApp(
+#'     ui = f7Page(
+#'       color = "pink",
+#'       title = "My app",
+#'       f7Init("ios"),
+#'       f7Sheet(
+#'         id = "sheet1",
+#'         label = "More",
+#'         orientation = "bottom",
+#'         "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#'         Quisque ac diam ac quam euismod porta vel a nunc. Quisque sodales
+#'         scelerisque est, at porta justo cursus ac"
+#'       )
+#'     ),
+#'     server = function(input, output) {}
+#'   )
+#' }
+f7Sheet <- function(..., id, label = "Open", orientation = c("top", "bottom")) {
+
+  orientation <- match.arg(orientation)
+
+  sheetCl <- "sheet-modal "
+  if (orientation == "top") sheetCl <- paste0(sheetCl, " sheet-modal-top")
+
+
+ shiny::tagList(
+   # javascript initialization
+   shiny::singleton(
+     tags$head(
+       tags$script(
+         "$(function() {
+          var sheet = app.sheet.create({
+            el: '#, ", id, "'
+          });
+        });
+        "
+       )
+     )
+   ),
+   # sheet tag
+   shiny::a(class = "button button-fill sheet-open", href="#", `data-sheet` = paste0("#", id), label),
+   shiny::tags$div(
+     class = sheetCl,
+     id = id,
+     shiny::tags$div(
+       class = "toolbar",
+       shiny::tags$div(
+         class = "toolbar-inner",
+         shiny::tags$div(class = "left"),
+         shiny::tags$div(
+           class = "right",
+           shiny::a(class = "link sheet-close", href = "#", "Done")
+          )
+       )
+     ),
+     shiny::tags$div(
+       class = "sheet-modal-inner",
+       shiny::tags$div(
+         class = "block",
+         ...
+       )
+     )
+   )
+ )
+}
