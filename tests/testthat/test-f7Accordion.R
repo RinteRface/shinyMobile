46177a9457a0a0159149013faa6589c8dd201dd3
DivadNojnarg <dgranjon@ymail.com>
2020-02-06 18:52:44 +0100

    test updateF7Accordion

--- tests/testthat/test-f7Accordion.R
+++ tests/testthat/test-f7Accordion.R
@@ -43,5 +43,22 @@ test_that("accordion items", {
   expect_equal(item[[1]]$children[[1]]$children[[1]]$attribs$class, "item-title")
   expect_equal(item[[2]]$name, "div")
   expect_equal(item[[2]]$attribs$class, "accordion-item-content")
+})
+
+
+test_that("update", {
+
+  session <- as.environment(list(
+    ns = identity,
+    sendInputMessage = function(inputId, message) {
+      session$lastInputMessage = list(id = inputId, message = message)
+    }
+  ))
+
+  updateF7Accordion(session = session, inputId = "accordion", selected = 1)
+  result <- session$lastInputMessage
+
+  expect_equal(result$message$selected, 1)
+  expect_equal(result$id, "accordion")
 
 })
