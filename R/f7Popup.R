fb58ee65ae2cce819ff1956e07bdb0e37531bd90
pvictor <perrier.victor@gmail.com>
2020-02-09 10:50:37 +0100

    updated f7Popup

--- R/f7Popup.R
+++ R/f7Popup.R
@@ -1,14 +1,25 @@
-#' Create a f7 popup
+#' @title Create a f7 popup
 #'
-#' @param ... Content.
+#' @description Popup is a popup window with any UI content that pops up over App's main content.
+#'  Popup as all other overlays is part of so called "Temporary Views".
+#'
+#' @param ... UI elements for the body of the popup window.
 #' @param id Popup unique id.
-#' @param title Title.
-#' @param backdrop Enables Popup backdrop (dark semi transparent layer behind). Default to TRUE.
-#' @param closeByBackdropClick When enabled, popup will be closed on backdrop click. Default to TRUE.
-#' @param closeOnEscape When enabled, popup will be closed on ESC keyboard key press. Default to FALSE.
-#' @param animate Whether the Popup should be opened/closed with animation or not. Default to TRUE.
-#' @param swipeToClose Whether the Popup can be closed with swipe gesture. Can be true to allow to close popup with swipes to top and to bottom.
-#' Default to FALSE.
+#' @param title Title for the popup window, use \code{NULL} for no title.
+#' @param backdrop Enables Popup backdrop (dark semi transparent layer behind).
+#'  Default to \code{TRUE}.
+#' @param closeByBackdropClick When enabled, popup will be closed on backdrop click.
+#'  Default to \code{TRUE}.
+#' @param closeOnEscape When enabled, popup will be closed on ESC keyboard key press.
+#'  Default to \code{FALSE}.
+#' @param animate Whether the Popup should be opened/closed with animation or not.
+#'  Default to \code{TRUE}.
+#' @param swipeToClose Whether the Popup can be closed with swipe gesture.
+#'  Can be true to allow to close popup with swipes to top and to bottom.
+#'  Default to \code{FALSE}.
+#' @param fullsize Open popup in full width or not. Default to \code{FALSE}.
+#' @param closeButton Add or not a button to easily close the popup.
+#'  Default to \code{TRUE}.
 #'
 #' @export
 #'
@@ -56,37 +67,58 @@
 #'    }
 #'  )
 #' }
-f7Popup <- function(..., id, title, backdrop = TRUE, closeByBackdropClick = TRUE,
-                    closeOnEscape = FALSE, animate = TRUE, swipeToClose = FALSE) {
+f7Popup <- function(..., id, title = NULL,
+                    backdrop = TRUE,
+                    closeByBackdropClick = TRUE,
+                    closeOnEscape = FALSE,
+                    animate = TRUE,
+                    swipeToClose = FALSE,
+                    fullsize = FALSE,
+                    closeButton = TRUE) {
 
-  popupProps <- dropNulls(
-    list(
-      class = "popup popup-tablet-fullscreen",
-      id = id,
-      style = "overflow-y: auto;",
-      `data-backdrop` = tolower(backdrop),
-      `data-close-by-backdrop-click` = tolower(closeByBackdropClick),
-      `data-close-on-escape` = tolower(closeOnEscape),
-      `data-animate` = tolower(animate),
-      `data-swipe-to-close` = tolower(swipeToClose)
-    )
+  config <- dropNulls(list(
+    backdrop = backdrop,
+    closeByBackdropClick = closeByBackdropClick,
+    closeOnEscape = closeOnEscape,
+    animate = animate,
+    swipeToClose = swipeToClose
+  ))
+
+  content <- shiny::tags$div(
+    class = "block",
+    if (!is.null(title)) shiny::tags$div(class = "block-title", title),
+    ...
   )
 
-  popupTag <- do.call(shiny::tags$div, popupProps)
-  popupWrap <- shiny::tagAppendChildren(
-    popupTag,
+  if (isTRUE(closeButton)) {
+    content <- htmltools::tagAppendChild(
+      content,
+      shiny::tags$a(
+        class = "link popup-close",
+        style = "position: absolute; top: -15px; right: 10px;",
+        href = "#",
+        f7Icon("close")
+      )
+    )
+  }
+
+  shiny::tags$div(
+    id = id,
+    class = "popup",
+    class = if (isTRUE(fullsize)) "popup-tablet-fullscreen",
+    style = "overflow-y: auto;",
     f7InputsDeps(),
-    shiny::br(),
-    shiny::br(),
-    shiny::div(
-      class = "block",
-      shiny::p(title),
-      shiny::p(shiny::a(class = "link popup-close", href = "#", "Close")),
-      shiny::p(...)
+    content,
+    shiny::tags$script(
+      type = "application/json",
+      `data-for` = id,
+      jsonlite::toJSON(
+        x = config,
+        auto_unbox = TRUE,
+        json_verbatim = TRUE
+      )
     )
   )
-
-  popupWrap
 }
 
 
