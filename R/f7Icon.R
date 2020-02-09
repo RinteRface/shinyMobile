f507927166f6faa1d701b2f429483a5f6a9b5dd9
pvictor <perrier.victor@gmail.com>
2020-02-09 11:49:08 +0100

    new icons

--- R/f7Icon.R
+++ R/f7Icon.R
@@ -1,13 +1,20 @@
-#' Create a Framework7 icon
+#' @title Framework7 icons
 #'
-#' Build a Framework7 icon
+#' @description Use Framework7 icons in shiny applications,
+#'  see complete list of icons here : \url{https://framework7.io/icons/}.
 #'
 #' @param ... Icon name and \link{f7Badge}.
-#' @param lib Library to use: NULL, "ios" or "md". Leave NULL by default. Specify, md or ios
-#' if you want to hide/show icons on specific devices.
+#' @param lib Library to use: NULL, "ios" or "md". Leave \code{NULL} by default. Specify, md or ios
+#'  if you want to hide/show icons on specific devices.
 #' @param color Icon color, if any.
+#' @param old Use \code{TRUE} for old icons, default is \code{TRUE},
+#'  but in a future version of shinyMobile default will be \code{FALSE}.
 #'
 #' @examples
+#'
+#' f7Icon("close", old = TRUE)
+#' f7Icon("multiply", old = FALSE)
+#'
 #' if(interactive()){
 #'  library(shiny)
 #'  library(shinyMobile)
@@ -46,17 +53,36 @@
 #' @author David Granjon, \email{dgranjon@@ymail.com}
 #'
 #' @export
-f7Icon <- function(..., lib = NULL, color = NULL) {
+f7Icon <- function(..., lib = NULL, color = NULL, old = TRUE) {
+  call_ <- as.list(match.call())
+  if (is.null(call_$old)) {
+    warning(
+      "In a future version of shinyMobile, default for ",
+      "old argument will be FALSE, to continue to use old icons ",
+      "specify explicitly old = TRUE."
+    )
+  }
   if (!is.null(lib)) {
-    if (lib == "ios") iconCl <- "icon f7-icons ios-only"
-    if (lib == "md") iconCl <- "icon material-icons md-only"
+    if (identical(lib, "ios")) {
+      iconCl <- "icon f7-icons ios-only"
+    }
+    if (identical(lib, "md")) {
+      iconCl <- "icon material-icons md-only"
+    }
   } else {
     # class icon is necessary so that icons with labels render well,
     # for instance
     iconCl <- "icon f7-icons"
   }
 
-  if (!is.null(color)) iconCl <- paste0(iconCl, " color-", color)
+  if (!is.null(color)) {
+    iconCl <- paste0(iconCl, " color-", color)
+  }
 
-  shiny::tags$i(class = iconCl, ...)
+  iconTag <- htmltools::attachDependencies(
+    x = shiny::tags$i(class = iconCl, ...),
+    value = html_dependencies_f7Icons(old = old)
+  )
+  htmltools::browsable(iconTag)
 }
+
