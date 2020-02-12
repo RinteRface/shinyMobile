8bc55d907dbbe2c66cd60183bbd974e78b3944b5
DivadNojnarg <dgranjon@ymail.com>
2020-02-12 09:09:04 +0100

    add more tests for buttons

--- tests/testthat/test-f7Button.R
+++ tests/testthat/test-f7Button.R
@@ -13,5 +13,15 @@ test_that("button", {
   # class
   expect_equal(f7Button()[[2]]$attribs$class, "button button-fill")
   expect_equal(f7Button(fill = FALSE)[[2]]$attribs$class, "button")
+  expect_equal(
+    f7Button(fill = FALSE, outline = TRUE)[[2]]$attribs$class,
+    "button button-outline"
+  )
+  expect_equal(f7Button(shadow = TRUE)[[2]]$attribs$class, "button button-fill button-raised")
+  expect_equal(f7Button(rounded = TRUE)[[2]]$attribs$class, "button button-fill button-round")
+  expect_equal(f7Button(size = "small")[[2]]$attribs$class, "button button-fill button-small")
+  expect_equal(f7Button(color = "pink")[[2]]$attribs$class, "button color-pink button-fill")
 
+  # input binding class
+  expect_equal(f7Button(inputId = "test")[[2]]$attribs$class, "button f7-action-button button-fill")
 })
