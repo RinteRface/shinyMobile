51096b3387fa2751390c4b83265a99b2599a9f09
DivadNojnarg <dgranjon@ymail.com>
2020-03-06 01:03:40 +0100

    add messages js rules

--- inst/framework7-5.3.0/input-bindings/messagesInputBinding.js
+++ inst/framework7-5.3.0/input-bindings/messagesInputBinding.js
@@ -12,9 +12,49 @@ $.extend(f7MessagesBinding, {
     config = JSON.parse(config.html());
 
     config.el = '#' + id;
+
+    // add other methods
+// First message rule
+  config.firstMessageRule = function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+    /* if:
+      - there is no previous message
+      - or previous message type (send/received) is different
+      - or previous message sender name is different
+    */
+    if (!previousMessage || previousMessage.type !== message.type || previousMessage.name !== message.name) return true;
+    return false;
+  };
+
+  // Last message rule
+  config.lastMessageRule = function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+    /* if:
+      - there is no next message
+      - or next message type (send/received) is different
+      - or next message sender name is different
+    */
+    if (!nextMessage || nextMessage.type !== message.type || nextMessage.name !== message.name) return true;
+    return false;
+  };
+
+  // Last message rule
+  config.tailMessageRule = function (message, previousMessage, nextMessage) {
+    // Skip if title
+    if (message.isTitle) return false;
+      /* if (bascially same as lastMessageRule):
+      - there is no next message
+      - or next message type (send/received) is different
+      - or next message sender name is different
+    */
+    if (!nextMessage || nextMessage.type !== message.type || nextMessage.name !== message.name) return true;
+    return false;
+  };
+
     // feed the create method
     var messages = app.messages.create(config);
-    console.log(messages);
   },
 
   find: function(scope) {
@@ -33,7 +73,16 @@ $.extend(f7MessagesBinding, {
 
   // see updatef7Messages
   receiveMessage: function(el, data) {
+    console.log(data);
     var messages = app.messages.get($(el));
+    setTimeout(function() {
+      messages.showTyping({
+        header: data.name + ' is typing',
+        avatar: data.avatar
+      });
+    }, 1000);
+    messages.addMessage(data);
+    messages.hideTyping();
   },
 
   subscribe: function(el, callback) {
