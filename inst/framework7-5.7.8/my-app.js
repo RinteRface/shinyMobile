e443620b16a3fd8e0d355c1c9692730f14604d74
DivadNojnarg <dgranjon@ymail.com>
2021-01-05 22:31:15 +0100

    definitely remove old workerId code

--- inst/framework7-5.7.8/my-app.js
+++ inst/framework7-5.7.8/my-app.js
@@ -1,15 +1,4 @@
 $(function () {
-  // handles shinyapps.io
-  //var workerId = $('base').attr('href');
-  //// ensure that this code does not locally
-  //if (typeof workerId != "undefined") {
-  //  var hash = window.location.hash;
-  //  var search = window.location.search;
-  //  var pathname = window.location.pathname;
-  //  var newpath = pathname + workerId + search + hash;
-  //  window.history.replaceState( {} , 'newpath', newpath);
-  //}
-
   // show reconnect notification
   $(document).on('shiny:disconnected', function(event) {
     // remove shiny server stuff
