b6bcf982314caa838a81c90aa71154096c0462c7
Veerle van Leemput <info@hypebright.nl>
2024-03-08 14:18:23 +0100

    change `root` to `el`

--- srcjs/helpers/helpers-config.js
+++ srcjs/helpers/helpers-config.js
@@ -1,24 +1,24 @@
 import { createStore } from 'framework7';
- 
+
 export const setConfig = () => {
   // collect all data elements stored in body
   let config = $(document).find("script[data-for='app']");
   config = JSON.parse(config.html());
-  
+
   // always erase existing root value just in case the user changes the root.
   // This may be harmful
-  config.root = "#app";
-  
+  config.el = "#app";
+
   // check if the app is intended to be a PWA
   let isPWA = $('body').attr('data-pwa') === "true";
-  
+
   if (isPWA) {
     config.serviceWorker = {
       path: window.location.pathname + "service-worker.js",
       scope: window.location.pathname
     };
   }
-  
+
   // Widgets cache
   config.store = createStore({
     state: {
@@ -35,6 +35,6 @@ export const setConfig = () => {
       //}
     }
   });
-  
+
   return config;
 };
