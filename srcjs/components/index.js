a74b0d9b3c4cca9167058080b44bde13da976156
DivadNojnarg <dgranjon@ymail.com>
2024-03-10 23:51:17 +0100

    add index to components and bindings

---
+++ srcjs/components/index.js
@@ -0,0 +1,15 @@
+import './init.js';
+import './actionSheet.js';
+import './chips.js';
+import './dialog.js';
+import './gauge.js';
+import './navbar.js';
+import './popover.js';
+import './preloader.js';
+import './progress.js';
+import './skeleton.js';
+import './subNavbar.js';
+import './swipeout.js';
+import './tabs.js';
+import './tapHold.js';
+import './tooltip.js';
