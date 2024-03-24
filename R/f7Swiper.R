62e11e7a604e2a62dba1600aa7f537141a22c0b6
DivadNojnarg <dgranjon@ymail.com>
2024-03-24 22:40:02 +0100

    test/check f7Swiper

--- R/f7Swiper.R
+++ R/f7Swiper.R
@@ -9,125 +9,29 @@
 #'
 #' @rdname swiper
 #'
-#' @examples
-#' if(interactive()){
-#'  library(shiny)
-#'  library(shinyMobile)
-#'
-#'  timeline <- f7Timeline(
-#'   sides = TRUE,
-#'   f7TimelineItem(
-#'    "Another text",
-#'    date = "01 Dec",
-#'    card = FALSE,
-#'    time = "12:30",
-#'    title = "Title",
-#'    subtitle = "Subtitle",
-#'    side = "left"
-#'   ),
-#'   f7TimelineItem(
-#'    "Another text",
-#'    date = "02 Dec",
-#'    card = TRUE,
-#'    time = "13:00",
-#'    title = "Title",
-#'    subtitle = "Subtitle"
-#'   ),
-#'   f7TimelineItem(
-#'    "Another text",
-#'    date = "03 Dec",
-#'    card = FALSE,
-#'    time = "14:45",
-#'    title = "Title",
-#'    subtitle = "Subtitle"
-#'   )
-#'  )
-#'
-#'  shiny::shinyApp(
-#'    ui = f7Page(
-#'     title = "Swiper",
-#'     f7SingleLayout(
-#'      navbar = f7Navbar(title = "f7Swiper"),
-#'      f7Swiper(
-#'       id = "my-swiper",
-#'       f7Slide(
-#'        timeline
-#'       ),
-#'       f7Slide(
-#'        f7Toggle(
-#'         inputId = "toggle",
-#'         label = "My toggle",
-#'         color = "pink",
-#'         checked = TRUE
-#'        ),
-#'        verbatimTextOutput("test")
-#'       ),
-#'       f7Slide(
-#'        f7Slider(
-#'         inputId = "obs",
-#'         label = "Number of observations",
-#'         max = 1000,
-#'         min = 0,
-#'         value = 100,
-#'         scaleSteps = 5,
-#'         scaleSubSteps = 3,
-#'         scale = TRUE,
-#'         color = "orange",
-#'         labels = tagList(
-#'           f7Icon("circle"),
-#'           f7Icon("circle_fill")
-#'         )
-#'        ),
-#'        plotOutput("distPlot")
-#'       )
-#'      )
-#'     )
-#'    ),
-#'    server = function(input, output) {
-#'     output$test <- renderPrint(input$toggle)
-#'     output$distPlot <- renderPlot({
-#'     hist(rnorm(input$obs))
-#'    })
-#'    }
-#'  )
-#' }
-#'
+#' @example inst/examples/swiper/app.R
 #' @author David Granjon, \email{dgranjon@@ymail.com}
 #'
 #' @export
 f7Swiper <- function(
-  ...,
-  id,
-  options = list(
-    speed = 400,
-    loop = FALSE,
-    spaceBetween = 50,
-    slidesPerView = "auto",
-    centeredSlides = TRUE,
-    navigation = list(
-      nextEl = ".swiper-button-next",
-      prevEl = ".swiper-button-prev"
-    ),
-    pagination = list(el = ".swiper-pagination", clickable = TRUE),
-    scrollbar = list(el = ".swiper-scrollbar", draggable = TRUE)
-  )
-) {
-  # swiper class
-  swiperCl <- "swiper swiper-container"
-
-  swiper_config <- shiny::tags$script(
-    type = "application/json",
-    `data-for` = id,
-    jsonlite::toJSON(
-      x = options,
-      auto_unbox = TRUE,
-      json_verbatim = TRUE
-    )
-  )
-
+    ...,
+    id,
+    options = list(
+      speed = 400,
+      loop = FALSE,
+      spaceBetween = 50,
+      slidesPerView = "auto",
+      centeredSlides = TRUE,
+      navigation = list(
+        nextEl = ".swiper-button-next",
+        prevEl = ".swiper-button-prev"
+      ),
+      pagination = list(el = ".swiper-pagination", clickable = TRUE),
+      scrollbar = list(el = ".swiper-scrollbar", draggable = TRUE)
+    )) {
   # swiper tag
   shiny::tags$div(
-    class = swiperCl,
+    class = "swiper",
     id = id,
     shiny::tags$div(
       class = "swiper-wrapper",
@@ -135,26 +39,22 @@ f7Swiper <- function(
     ),
     if (!is.null(options$pagination)) shiny::tags$div(class = "swiper-pagination"),
     if (!is.null(options$navigation)) {
-      list(
-        shiny::tags$div(class ="swiper-button-prev"),
-        shiny::tags$div(class ="swiper-button-next")
+      shiny::tagList(
+        shiny::tags$div(class = "swiper-button-prev"),
+        shiny::tags$div(class = "swiper-button-next")
       )
     },
     if (!is.null(options$scrollbar)) shiny::tags$div(class = "swiper-scrollbar"),
-    swiper_config
+    buildConfig(id, options)
   )
 }
-
-
-
-
 
 #' Framework7 slide
 #'
 #' \code{f7Slide} is an \link{f7Swiper} element.
 #'
 #' @param ... Slide content. Any element.
-#'
+#' @rdname swiper
 #' @export
 f7Slide <- function(...) {
   shiny::tags$div(class = "swiper-slide", ...)
