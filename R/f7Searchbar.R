635f2f6141a53fee202d6a23791165100044d03e
DivadNojnarg <dgranjon@ymail.com>
2024-03-24 09:40:24 +0100

    more tests for searchbar + doc tweaks

--- R/f7Searchbar.R
+++ R/f7Searchbar.R
@@ -15,6 +15,7 @@
 #' item title. This may be changed by updating the default searchContainer and
 #' searchIn.
 #' @export
+#' @rdname searchbar
 #'
 #' @examples
 #' if (interactive()) {
@@ -100,19 +101,22 @@ f7Searchbar <- function(id, placeholder = "Search", expandable = FALSE, inline =
     )
   )
 
+  searchBarTools <- shiny::tags$div(
+    class = "searchbar-input-wrap",
+    shiny::tags$input(type = "search", placeholder = placeholder),
+    shiny::tags$i(class = "searchbar-icon"),
+    shiny::tags$span(class = "input-clear-button")
+  )
+
   searchBarCl <- "searchbar"
-  if (expandable) searchBarCl <- paste0(searchBarCl, " searchbar-expandable")
+  if (inline) searchBarCl <- paste(searchBarCl, "searchbar-inline")
+  if (expandable) searchBarCl <- paste(searchBarCl, "searchbar-expandable")
 
   searchBarTag <- if (inline) {
     shiny::tags$div(
-      class = "searchbar searchbar-inline",
+      class = searchBarCl,
       id = id,
-      shiny::tags$div(
-        class = "searchbar-input-wrap",
-        shiny::tags$input(type = "search", placeholder = placeholder),
-        shiny::tags$i(class = "searchbar-icon"),
-        shiny::tags$span(class = "input-clear-button")
-      )
+      searchBarTools
     )
   } else {
     shiny::tags$form(
@@ -120,30 +124,22 @@ f7Searchbar <- function(id, placeholder = "Search", expandable = FALSE, inline =
       id = id,
       shiny::tags$div(
         class = "searchbar-inner",
-        shiny::tags$div(
-          class = "searchbar-input-wrap",
-          shiny::tags$input(type = "search", placeholder = placeholder),
-          shiny::tags$i(class = "searchbar-icon"),
-          shiny::tags$span(class = "input-clear-button")
-        ),
+        searchBarTools,
         shiny::tags$span(class = "searchbar-disable-button", "Cancel")
       )
     )
   }
 
-  shiny::tagList(
-    searchBarTag,
-    searchBarConfig
-  )
+  shiny::tagList(searchBarTag, searchBarConfig)
 }
-
-
 
 #' Framework 7 searchbar trigger
 #'
-#' Element that triggers the searchbar.
+#' \link{f7SearchbarTrigger}: Element that
+#' triggers the searchbar.
 #'
 #' @param targetId Id of the \link{f7Searchbar}.
+#' @rdname searchbar
 #' @export
 f7SearchbarTrigger <- function(targetId) {
   shiny::tags$a(
@@ -155,9 +151,11 @@ f7SearchbarTrigger <- function(targetId) {
 
 #' Utility to hide a given tag on search
 #'
-#' Use with \link{f7Searchbar}.
+#' \link{f7HideOnSearch}: elements with such class
+#' on page will be hidden during search
 #'
 #' @param tag tag to hide.
+#' @rdname searchbar
 #' @export
 f7HideOnSearch <- function(tag) {
   tag$attribs$class <- paste(tag$attribs$class, "searchbar-hide-on-search")
@@ -166,9 +164,11 @@ f7HideOnSearch <- function(tag) {
 
 #' Utility to hide a given tag when \link{f7Searchbar} is enabled.
 #'
-#' Use with \link{f7Searchbar}.
+#' \link{f7HideOnEnable}: elements with such class
+#' on page will be hidden when searchbar is enabled
 #'
 #' @param tag tag to hide.
+#' @rdname searchbar
 #' @export
 f7HideOnEnable <- function(tag) {
   tag$attribs$class <- paste(tag$attribs$class, "searchbar-hide-on-enable")
@@ -177,9 +177,12 @@ f7HideOnEnable <- function(tag) {
 
 #' Utility to display an item when the search is unsuccessful.
 #'
-#' Use with \link{f7Searchbar}.
+#' \link{f7SearchNotFound}: elements with such
+#' class are hidden by default and become visible
+#' when there is not any search results
 #'
 #' @param tag tag to use.
+#' @rdname searchbar
 #' @export
 f7NotFound <- function(tag) {
   tag$attribs$class <- paste(tag$attribs$class, "searchbar-not-found")
@@ -188,10 +191,13 @@ f7NotFound <- function(tag) {
 
 #' Utility to display an item when the search is successful.
 #'
-#' Use with \link{f7Searchbar}.
+#' \link{f7SearchFound}: elements with such
+#' class are visible by default and become hidden
+#' when there is not any search results.
 #'
 #' @param tag tag to display. When using \link{f7Searchbar}, one must
 #' wrap the items to search in inside \link{f7Found}.
+#' @rdname searchbar
 #' @export
 f7Found <- function(tag) {
   tag$attribs$class <- paste(tag$attribs$class, "searchbar-found")
@@ -200,9 +206,11 @@ f7Found <- function(tag) {
 
 #' Utility to ignore an item from search.
 #'
-#' Use with \link{f7Searchbar}.
+#' \link{f7SearchIgnore}: searchbar will not
+#' consider this elements in search results.
 #'
 #' @param tag tag to ignore.
+#' @rdname searchbar
 #' @export
 f7SearchIgnore <- function(tag) {
   tag$attribs$class <- paste(tag$attribs$class, "searchbar-ignore")
---
+++ tests/testthat/test-f7Searchbar.R
@@ -0,0 +1,68 @@
+test_that("searchbar tag works", {
+  searchbar <- f7Searchbar(
+    id = "test",
+    options = list(customSearch = TRUE)
+  )
+
+  config <- searchbar[[2]]
+  search_content <- searchbar[[1]]
+
+  expect_s3_class(searchbar, "shiny.tag.list")
+  expect_identical(config$attribs$`data-for`, "test")
+  expect_identical(
+    jsonlite::fromJSON(config$children[[1]]),
+    list(customSearch = TRUE)
+  )
+
+  expect_identical(search_content$name, "form")
+  expect_identical(search_content$attribs$class, "searchbar")
+  expect_identical(search_content$attribs$id, "test")
+
+  # Expandable
+  searchbar <- f7Searchbar(
+    id = "test",
+    expandable = TRUE
+  )
+
+  expect_true(grepl("searchbar-expandable", searchbar[[1]]$attribs$class))
+
+  # Inline
+  searchbar <- f7Searchbar(
+    id = "test",
+    inline = TRUE
+  )
+
+  expect_true(grepl("searchbar-inline", searchbar[[1]]$attribs$class))
+})
+
+test_that("searchbar trigger works", {
+  trigger <- f7SearchbarTrigger("test")
+  expect_identical(trigger$name, "a")
+  expect_identical(trigger$attribs$`data-searchbar`, "#test")
+  expect_true(grepl("searchbar-enable", trigger$attribs$class))
+})
+
+test_that("hide on search works", {
+  res <- f7HideOnSearch(tags$div())
+  expect_true(grepl("searchbar-hide-on-search", res$attribs$class))
+})
+
+test_that("hide on enable works", {
+  res <- f7HideOnEnable(tags$div())
+  expect_true(grepl("searchbar-hide-on-enable", res$attribs$class))
+})
+
+test_that("Not found works", {
+  res <- f7NotFound(tags$div())
+  expect_true(grepl("searchbar-not-found", res$attribs$class))
+})
+
+test_that("Found works", {
+  res <- f7Found(tags$div())
+  expect_true(grepl("searchbar-found", res$attribs$class))
+})
+
+test_that("Search ignore works", {
+  res <- f7SearchIgnore(tags$div())
+  expect_true(grepl("searchbar-ignore", res$attribs$class))
+})
