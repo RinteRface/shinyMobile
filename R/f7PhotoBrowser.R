1aa2d848d3b3ca1f79e04c65cad6525f7f660e6a
Veerle van Leemput <info@hypebright.nl>
2024-03-26 16:18:14 +0100

    start `f7PhotoBrowser`

--- R/f7PhotoBrowser.R
+++ R/f7PhotoBrowser.R
@@ -5,40 +5,13 @@
 #' @param photos List of photos
 #' @param theme Browser theme: choose either light or dark.
 #' @param type Browser type: choose among \code{c("popup", "standalone", "page")}.
-#' @param ... Other options.
+#' @param ... Other options to pass to the photo browser. See
+#' \url{https://framework7.io/docs/photo-browser#photo-browser-parameters} for more details.
 #' @param session Shiny session object.
 #'
+#' @example inst/examples/photobrowser/app.R
+#'
 #' @export
-#'
-#' @examples
-#' if (interactive()) {
-#'   library(shiny)
-#'   library(shinyMobile)
-#'
-#'   shinyApp(
-#'     ui = f7Page(
-#'       title = "f7PhotoBrowser",
-#'       f7SingleLayout(
-#'         navbar = f7Navbar(title = "f7PhotoBrowser"),
-#'         f7Button(inputId = "togglePhoto", "Open photo")
-#'       )
-#'     ),
-#'     server = function(input, output, session) {
-#'       observeEvent(input$togglePhoto, {
-#'         f7PhotoBrowser(
-#'           id = "photobrowser1",
-#'           theme = "dark",
-#'           type = "standalone",
-#'           photos = c(
-#'             "https://cdn.framework7.io/placeholder/sports-1024x1024-1.jpg",
-#'             "https://cdn.framework7.io/placeholder/sports-1024x1024-2.jpg",
-#'             "https://cdn.framework7.io/placeholder/sports-1024x1024-3.jpg"
-#'           )
-#'         )
-#'       })
-#'     }
-#'   )
-#' }
 f7PhotoBrowser <- function(photos, theme = c("light", "dark"),
                            type = c("popup", "standalone", "page"), ..., session = shiny::getDefaultReactiveDomain()) {
   theme <- match.arg(theme)
