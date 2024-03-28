041437c43eb8f4886a79512713466de192900855
Veerle van Leemput <info@hypebright.nl>
2024-03-28 10:58:06 +0100

    start validateF7Input

---
+++ inst/examples/validateinput/app.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(shinyMobile)
+
+app <- shinyApp(
+  ui = f7Page(
+    title = "Validate inputs",
+    f7SingleLayout(
+      navbar = f7Navbar(title = "validateF7Input"),
+      f7Text(
+        inputId = "caption",
+        label = "Caption",
+        value = "Data Summary"
+      ),
+      verbatimTextOutput("value"),
+      hr(),
+      f7Text(
+        inputId = "caption2",
+        label = "Enter a number",
+        value = 1
+      ),
+      hr(),
+      f7Password(
+        inputId = "password",
+        label = "Password"
+      ),
+      hr(),
+      f7Select(
+        inputId = "select",
+        label = "Select",
+        choices = c("A", "B", "C"),
+        selected = character(0)
+      )
+    )
+  ),
+  server = function(input, output, session) {
+
+    observe({
+      validateF7Input(inputId = "caption", info = "Whatever")
+      validateF7Input(
+        inputId = "caption2",
+        pattern = "[0-9]*",
+        error = "Only numbers please!"
+      )
+      validateF7Input(
+        inputId = "password",
+        pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$",
+        error = "Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+      )
+    })
+
+    output$value <- renderPrint({
+      input$caption
+    })
+  }
+)
+
+if (interactive() || identical(Sys.getenv("TESTTHAT"), "true")) app
