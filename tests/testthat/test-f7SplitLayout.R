76f1cc61bf8c424096213cb769fd30b3ad7c9d59
Veerle van Leemput <info@hypebright.nl>
2024-04-03 15:31:38 +0200

    `f7SplitLayout` test

---
+++ tests/testthat/test-f7SplitLayout.R
@@ -0,0 +1,20 @@
+library(shinytest2)
+test_that("Split layout works as expected", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
+  shiny_app_path <-
+    system.file("examples/split_layout/app.R", package = "shinyMobile")
+  app <- AppDriver$new(
+    shiny_app_path,
+    name = "splitlayout-app",
+    variant = platform_variant(),
+    width = 1024,
+    height = 768
+  )
+  # Animation/transition takes a bit of time
+  app$wait_for_idle(1000)
+  app$expect_values(input = "menu")
+  app$click(selector = '#menu a.tab-link[data-tab="#tab2"]')
+  app$expect_values(input = "menu")
+  app$stop()
+})
