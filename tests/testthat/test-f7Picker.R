968dd1a5bac6810b970d6693b0c4a72cdb45ef6b
Veerle van Leemput <info@hypebright.nl>
2024-04-20 11:51:11 +0200

    update picker example + test

--- tests/testthat/test-f7Picker.R
+++ tests/testthat/test-f7Picker.R
@@ -9,7 +9,6 @@ test_that("picker works", {
     variant = platform_variant()
   )
 
-  # Disable popovers
   app$expect_values(input = "picker")
   app$click(selector = "#picker")
   app$wait_for_idle(2000)
@@ -18,6 +17,10 @@ test_that("picker works", {
   app$click(selector = "#picker")
   app$wait_for_idle(2000)
   app$expect_values(input = "picker")
+  app$click(selector = ".tab-link:first-child")
+  app$click(selector = ".tab-link:not(.tab-link-active)")
+  app$wait_for_idle(2000)
+  app$expect_values(input = "picker2")
 })
 
 test_that("picker tag works", {
@@ -39,4 +42,26 @@ test_that("picker tag works", {
     selectedTags()[[1]]
   expect_identical(input$attribs$class, "picker-input")
   expect_identical(input$attribs$id, "picker")
+})
+
+test_that("picker in list works", {
+  picker <- f7List(
+    f7Picker(
+      inputId = "picker",
+      label = "Picker Input",
+      choices = c("a", "b", "c"),
+      options = list(sheetPush = TRUE)
+    )
+  )
+
+  expect_s3_class(picker, "shiny.tag")
+  config <- htmltools::tagQuery(picker)$
+    find("script")$
+    selectedTags()[[1]]
+  expect_identical(config$attribs$`data-for`, "picker")
+
+  label <- htmltools::tagQuery(picker)$
+    find(".item-title")$
+    selectedTags()[[1]]
+  expect_identical(label$attribs$class, "item-title item-label")
 })
