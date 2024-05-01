39cd327be7d6446509f4efa1d156240fddafa26b
Veerle van Leemput <info@hypebright.nl>
2024-05-01 18:17:12 +0200

    wait for idle

--- tests/testthat/test-f7VirtualList.R
+++ tests/testthat/test-f7VirtualList.R
@@ -13,6 +13,7 @@ test_that("virtualList works as expected", {
   app$expect_values(input = "vlist")
   app$click(selector = "#prependItems")
   app$click(selector = "#moveItem")
+  app$wait_for_idle(1000)
   app$expect_values(input = "vlist")
   # Set search bar value
   app$wait_for_js("var searchbarEl = document.getElementById('search1'); app.searchbar.search(searchbarEl, 'Title 1000')")
