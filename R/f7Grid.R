c9075e0be8956d52b802178878bed10253dda701
Thomas Charlon <2394508+ThomasChln@users.noreply.github.com>
2024-11-20 10:20:14 -0500

    Update f7Grid.R

--- R/f7Grid.R
+++ R/f7Grid.R
@@ -5,11 +5,17 @@
 #' @param ... Row content.
 #' @param cols Columns number. Numeric between 1 and 20.
 #' @param gap Whether to display gap between columns. TRUE by default.
+#' @param medium_cols Columns number for large screens. Numeric between 1 and 20.
 #'
 #' @author David Granjon, \email{dgranjon@@ymail.com}
 #' @export
-f7Grid <- function(..., cols, gap = TRUE) {
+f7Grid <- function(..., cols, gap = TRUE, medium_cols = NULL) {
   cl <- sprintf("grid grid-cols-%s", cols)
+                                                                                  
+  if (!is.null(medium_cols)) {                                                  
+    cl <- paste(cl, sprintf("medium-grid-cols-%s", medium_cols))                
+  }                                                                             
+                                                                                
   if (gap) cl <- paste(cl, "grid-gap")
   shiny::tags$div(class = cl, ...)
 }
