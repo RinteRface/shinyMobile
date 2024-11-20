db8fa8679d66b164f16d5ec03ee14d2159f4a9f7
Thomas Charlon <2394508+ThomasChln@users.noreply.github.com>
2024-11-20 10:17:18 -0500

    Update test-f7Grid.R

--- tests/testthat/test-f7Grid.R
+++ tests/testthat/test-f7Grid.R
@@ -5,4 +5,7 @@ test_that("grid works", {
 
   grid <- f7Grid(cols = 2, gap = FALSE)
   expect_identical(grid$attribs$class, "grid grid-cols-2")
+  
+  grid <- f7Grid(cols = 2, medium_cols = 4)
+  expect_identical(grid$attribs$class, "grid grid-cols-2 medium-grid-cols-4")
 })
