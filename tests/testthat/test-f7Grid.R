1846ca94406795c5eddfd51772c56061c2b5c178
Thomas Charlon <charlon@protonmail.com>
2024-11-30 13:27:51 -0500

    responsive f7Grid

--- tests/testthat/test-f7Grid.R
+++ tests/testthat/test-f7Grid.R
@@ -6,6 +6,20 @@ test_that("grid works", {
   grid <- f7Grid(cols = 2, gap = FALSE)
   expect_identical(grid$attribs$class, "grid grid-cols-2")
   
-  grid <- f7Grid(cols = 2, medium_cols = 4)
-  expect_identical(grid$attribs$class, "grid grid-cols-2 medium-grid-cols-4")
+
+  # responsiveCl
+  grid <- f7Grid(cols = 2, responsiveCl = 'xsmall-1')
+  expect_identical(grid$attribs$class, "grid grid-cols-2 xsmall-grid-cols-1 grid-gap")
+  
+  grid <- f7Grid(cols = 2, responsiveCl = 'small-2')
+  expect_identical(grid$attribs$class, "grid grid-cols-2 small-grid-cols-2 grid-gap")
+  
+  grid <- f7Grid(cols = 2, responsiveCl = 'medium-4')
+  expect_identical(grid$attribs$class, "grid grid-cols-2 medium-grid-cols-4 grid-gap")
+  
+  grid <- f7Grid(cols = 2, responsiveCl = 'large-6')
+  expect_identical(grid$attribs$class, "grid grid-cols-2 large-grid-cols-6 grid-gap")
+  
+  grid <- f7Grid(cols = 2, responsiveCl = 'xlarge-8')
+  expect_identical(grid$attribs$class, "grid grid-cols-2 xlarge-grid-cols-8 grid-gap")
 })
